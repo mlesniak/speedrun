591b6c419a512b7aa6f17fee2cfe913063c9f4a5
Michael Lesniak <mail@mlesniak.com>
2020-01-22 12:44:31 +0100

    Add basic collision detection

---
+++ go.mod
@@ -0,0 +1,12 @@
+module speedrun
+
+go 1.13
+
+require (
+	github.com/SolarLune/resolv v0.0.0-20190821203317-2f6176d8d107
+	github.com/gen2brain/raylib-go v0.0.0-20200122101733-6dc630b9be2a // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+	github.com/hajimehoshi/ebiten v1.10.3
+	github.com/veandco/go-sdl2 v0.4.0 // indirect
+	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8
+)
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/SolarLune/resolv/resolv"
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 	"image/color"
@@ -9,20 +10,23 @@ import (
 )
 
 type Object struct {
-	x, y float64
-	gray uint8
+	x, y  float64
+	gray  uint8
+	shape resolv.Rectangle
 }
 
 var player = Object{
-	x:    0,
-	y:    240,
-	gray: 40,
+	x:     0,
+	y:     240,
+	gray:  40,
+	shape: *resolv.NewRectangle(0, 240, 50, 50),
 }
 
 var obstacle = Object{
-	x:    600 - 50,
-	y:    240,
-	gray: 80,
+	x:     600 - 50,
+	y:     240,
+	gray:  80,
+	shape: *resolv.NewRectangle(600-50, 240, 50, 50),
 }
 
 func main() {
@@ -32,8 +36,15 @@ func main() {
 }
 
 func update(screen *ebiten.Image) error {
-	dx := 10.0
+	dx := 20.0
+	// Check for collision.
+	collision := resolv.Resolve(&player.shape, &obstacle.shape, int32(dx), 0)
+	if collision.Colliding() {
+		dx = float64(collision.ResolveX)
+	}
+
 	player.x += dx
+	player.shape.X += int32(dx)
 
 	if ebiten.IsKeyPressed(ebiten.KeyEscape) || ebiten.IsKeyPressed(ebiten.KeyQ) {
 		os.Exit(0)
